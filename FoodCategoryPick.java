0da13a803ab16d290041d19ce7bcc0746a9d17c7
LazarBajkic <113618364+LazarBajkic@users.noreply.github.com>
2023-01-01 21:15:58 +0100

    Category pick stylizing

--- FoodCategoryPick.java
+++ FoodCategoryPick.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.awt.Font;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,16 +10,16 @@ public class FoodCategoryPick extends JFrame{
     JButton fruitBtn=new JButton();
     JButton carbsBtn=new JButton();
     JButton sugarsBtn=new JButton();
-    JButton fatsBtn=new JButton();
     JButton meatBtn=new JButton();
     JButton finalizeBtn=new JButton();
     JLabel optional=new JLabel();
 
     FoodCategoryPick(){
-        this.setSize(500,500);
+        this.setSize(250,500);
         this.setLayout(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
 
         veggieBtn.setSize(130,25);
         veggieBtn.setLocation(50, 50);
@@ -31,7 +30,7 @@ public class FoodCategoryPick extends JFrame{
         veggieBtn.setForeground(Color.ORANGE);
 
         fruitBtn.setSize(130,25);
-        fruitBtn.setLocation(50, 80);
+        fruitBtn.setLocation(50, 100);
         fruitBtn.setFocusable(false);
         fruitBtn.setFont(new Font("Tahoma",Font.BOLD,13));
         fruitBtn.setText("Pick a fruit");
@@ -39,7 +38,7 @@ public class FoodCategoryPick extends JFrame{
         fruitBtn.setForeground(Color.orange);
 
         carbsBtn.setSize(130,25);
-        carbsBtn.setLocation(50, 110);
+        carbsBtn.setLocation(50, 150);
         carbsBtn.setFocusable(false);
         carbsBtn.setFont(new Font("Tahoma",Font.BOLD,12));
         carbsBtn.setText("Pick your carbs");
@@ -48,16 +47,32 @@ public class FoodCategoryPick extends JFrame{
 
         optional.setText("optional:");
         optional.setSize(70, 25);
-        optional.setLocation(50,130);
+        optional.setLocation(50,180);
         optional.setFont(new Font("Tahoma",Font.BOLD,12));
 
         sugarsBtn.setSize(130,25);
-        sugarsBtn.setLocation(50, 150);
+        sugarsBtn.setLocation(50, 210);
         sugarsBtn.setFocusable(false);
-        sugarsBtn.setFont(new Font("Tahoma",Font.BOLD,12));
+        sugarsBtn.setFont(new Font("Tahoma",Font.BOLD,11));
         sugarsBtn.setText("Pick your sugars");
         sugarsBtn.setBackground(Color.black);
         sugarsBtn.setForeground(Color.orange);
+
+        meatBtn.setSize(130,25);
+        meatBtn.setLocation(50, 260);
+        meatBtn.setFocusable(false);
+        meatBtn.setFont(new Font("Tahoma",Font.BOLD,11));
+        meatBtn.setText("Pick your meat");
+        meatBtn.setBackground(Color.black);
+        meatBtn.setForeground(Color.orange);
+
+        finalizeBtn.setSize(160,25);
+        finalizeBtn.setLocation(36, 350);
+        finalizeBtn.setFocusable(false);
+        finalizeBtn.setFont(new Font("Tahoma",Font.BOLD,11));
+        finalizeBtn.setText("Finalize and calculate");
+        finalizeBtn.setBackground(Color.black);
+        finalizeBtn.setForeground(Color.orange);
 
 
 
@@ -68,7 +83,6 @@ public class FoodCategoryPick extends JFrame{
         this.add(carbsBtn);
         this.add(optional);
         this.add(sugarsBtn);
-        this.add(fatsBtn);
         this.add(meatBtn);
         this.add(finalizeBtn);
     }
